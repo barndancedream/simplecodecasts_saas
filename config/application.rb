0ab5568237f8957c30b4356fcf00b7bef4bbd504
barndancedream <barndancedream@gmail.com>
2016-10-12 19:36:04 +0000

    commenting out line in config/application.rb

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,6 @@ module Workspace
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     #problem in line below?
-    #config.active_record.raise_in_transactional_callbacks = true
+    config.active_record.raise_in_transactional_callbacks = true
   end
 end
