f5e261a61f4b376151ce52ddf9135473aa5d8211
barndancedream <barndancedream@gmail.com>
2016-10-14 18:50:08 +0000

    Adding styles and icons

---
+++ app/helpers/users_helper.rb
@@ -0,0 +1,16 @@
+module UsersHelper
+    
+    def job_title_icon
+        if @user.profile.job_title == "Rapper"
+            "<i class='fa fa-code'></i>".html_safe
+            elsif  @user.profile.job_title == "Producer"
+            "<i class='fa fa-lightbulb-o'></i>".html_safe
+            elsif
+            @user.profile.job_title == "Investor"
+            "<i class='fa fa-dollar'></i>".html_safe
+                
+        
+       end
+    end
+end
+
